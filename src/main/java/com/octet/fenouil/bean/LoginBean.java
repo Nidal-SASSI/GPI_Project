3a4080d74509b9b318d7935c9dd6a90870f8a7ee
aghiles medane <aghilmdna9@gmail.com>
2021-03-17 01:03:59 +0100

    implementation of the Bean classes of custumorBean and productBean and loginBean

---
+++ src/main/java/com/octet/fenouil/bean/LoginBean.java
@@ -0,0 +1,45 @@
+package com.octet.fenouil.bean;
+
+public class LoginBean {private String login;
+private String password;
+private long role;
+private String confirmPassword;
+
+
+
+
+
+
+public String getConfirmPassword() {
+	return confirmPassword;
+}
+
+public void setConfirmPassword(String confirmPassword) {
+	this.confirmPassword = confirmPassword;
+}
+
+public long getRole() {
+	return role;
+}
+
+public void setRole(long role) {
+	this.role = role;
+}
+
+public String getLogin() {
+	return login;
+}
+
+public void setLogin(String login) {
+	this.login = login;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
